ded7fa22b23681ae3a24b0a7fdb3b3ad196d2431
Sterczik <sterczewskik@gmail.com>
2019-06-27 23:58:52 +0200

    Styles for SocialLogin buttons

--- src/components/SocialLogin/Facebook.js
+++ src/components/SocialLogin/Facebook.js
@@ -1,18 +1,33 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import FacebookLogin from 'react-facebook-login';
-import { authActions } from '../../containers/App/auth/actions';
+import React from 'react'
+import { connect } from 'react-redux'
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
+import {
+  Button
+} from "reactstrap"
+import { authActions } from '../../containers/App/auth/actions'
 
 const Facebook = (props) => (
   <FacebookLogin
     appId="300111870657527"
     fields="name,email,picture"
     callback={(response) => props.socialLogin(response, 'facebook')}
+    render={renderProps => (
+      <Button
+        className="btn-neutral btn-icon"
+        color="default"
+        onClick={renderProps.onClick}
+      >
+        <span className="btn-inner--icon mr-1">
+          <i className="fa fa-facebook" />
+        </span>
+        <span className="btn-inner--text">Facebook</span>
+      </Button>
+    )}
   />
-);
+)
 
 const mapDispatchToProps = (dispatch) => ({
   socialLogin: (response, provider) => dispatch(authActions.socialLogin(response, provider))
-});
+})
 
-export default connect(undefined, mapDispatchToProps)(Facebook);
+export default connect(undefined, mapDispatchToProps)(Facebook)
--- src/components/SocialLogin/Google.js
+++ src/components/SocialLogin/Google.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import GoogleLogin from 'react-google-login';
-import { authActions } from '../../containers/App/auth/actions';
+import React from 'react'
+import { connect } from 'react-redux'
+import GoogleLogin from 'react-google-login'
+import {
+  Button
+} from "reactstrap"
+import { authActions } from '../../containers/App/auth/actions'
 
 const Google = (props) => (
   <GoogleLogin
     clientId="156518502870-pdubhnv4g7vasi3sjdjhkltt1t2s3nrs.apps.googleusercontent.com"
-    buttonText="Login with Google"
+    render={renderProps => (
+      <Button
+        className="btn-neutral btn-icon ml-1"
+        color="default"
+        disabled={renderProps.disabled}
+        onClick={renderProps.onClick}
+      >
+        <span className="btn-inner--icon mr-1">
+          <img
+            alt="..."
+            src={require("../../assets/img/icons/common/google.svg")}
+          />
+        </span>
+        <span className="btn-inner--text">Google</span>
+      </Button>
+    )}
     onSuccess={(response) => props.socialLogin(response, 'google')}
     onFailure={(error) => console.log(error)}
   />
-);
+)
 
 const mapDispatchToProps = (dispatch) => ({
   socialLogin: (response, provider) => dispatch(authActions.socialLogin(response, provider))
-});
+})
 
-export default connect(undefined, mapDispatchToProps)(Google);
+export default connect(undefined, mapDispatchToProps)(Google)

2387101dd9b0d1b65bce62cae7b63f3cbc1f78b7
Sterczik <sterczewskik@gmail.com>
2020-05-20 21:05:39 +0200

    Remove useless helper

--- src/helpers/getUrlParameter.js
+++
@@ -1,8 +0,0 @@
-export default (name) => {
-  /* eslint-disable no-useless-escape, indent */
-  const names = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]')
-  /* eslint-disable prefer-template, indent */
-  const regex = new RegExp('[\\?&]' + names + '=([^&#]*)')
-  const results = regex.exec(window.location.search)
-  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '))
-}
